1b6e7e9fa673383c078f1f91770e412514d782e8
jparep <parepson@gmail.com>
2024-06-23 23:57:52 -0600

    SereisSerializer created

--- rodeo_app/rodeo/serializers.py
+++ rodeo_app/rodeo/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import Series, Rodeo, Contestant, Event, ContestEvent, EventPartner, MensBreakaway
+
+class SeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        module = Series
+        fields = '__all__'
+
