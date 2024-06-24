acab24de5f249beb5a5b4e8cb17f9715b724769d
jparep <parepson@gmail.com>
2024-06-24 02:39:17 -0600

    router registered

--- rodeo_app/rodeo/urls.py
+++ rodeo_app/rodeo/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SeriesViewSet,  SeriesViewSet, RodeoViewSet, ContestantViewSet, ContestRodeoSeriesViewSet, EventViewSet, ContestEventViewSet, EventPartnerViewSet, MensBreakawayViewSet
+
+router = DefaultRouter()
+router.register(r'series', SeriesViewSet)
+router.register(r'rodeo', RodeoViewSet)
+router.register(r'contestants', ContestantViewSet)
+router.register(r'contest-rodeo-series', ContestRodeoSeriesViewSet)
+router.register(r'events', EventViewSet)
+router.register(r'contest-events', ContestEventViewSet)
+router.register(r'event-partners', EventPartnerViewSet)
+router.register(r'mens-breakaway', MensBreakawayViewSet)
+
+urlpatterns = [path('', include(router.urls))]
