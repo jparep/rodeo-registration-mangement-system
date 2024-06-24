3fd1801c1e900df12f43c87a6613a37b9e73f419
jparep <parepson@gmail.com>
2024-06-24 01:47:40 -0600

    README.md updated

--- rodeo_app/rodeo_app/settings.py
+++ rodeo_app/rodeo_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'rodeo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rodeo_db',
+        'USER': 'rodeo_user',
+        'PASSWORD': 'Biripian1@2',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
