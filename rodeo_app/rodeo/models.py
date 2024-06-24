95d610a52e7c90f85cc2729dfc84d5f625f4d345
jparep <parepson@gmail.com>
2024-06-23 19:25:51 -0600

    Rodeo Model added

--- rodeo_app/rodeo/models.py
+++ rodeo_app/rodeo/models.py
@@ -16,3 +16,8 @@ class ContestRodeoSeries(models.Model):
     series = models.ForeignKey(Series, on_delete=models.CASCADE)
     class Meta:
         unique_together = (('contestant'), ('rodeo'), ('sereis'),)
+
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    is_timed = models.BooleanField()
