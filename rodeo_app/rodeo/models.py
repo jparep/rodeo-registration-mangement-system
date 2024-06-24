2c1873f05699397c7b845f389d690a6c787b099b
jparep <parepson@gmail.com>
2024-06-23 19:22:01 -0600

    Sereis Model Created

--- rodeo_app/rodeo/models.py
+++ rodeo_app/rodeo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Series(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Rodeo(models.Model):
+    name = models.CharField(max_length=50)
+
+class Contestant(models.Model):
+    name = models.CharField(max_length=100)
+    
+class ContestRodeoSeries(models.Model):
+    contstant = models.ForeignKey(Contestant, on_delete=models.CASCADE)
+    rodeo = models.ForeignKey(Rodeo, on_delete=models.CASCADE)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = (('contestant'), ('rodeo'), ('sereis'),)
