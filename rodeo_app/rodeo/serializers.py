4c724d004c5fe2712c0736987d7cbd40cdb3b3bd
jparep <parepson@gmail.com>
2024-06-24 00:09:51 -0600

    EventSerializer created

--- rodeo_app/rodeo/serializers.py
+++ rodeo_app/rodeo/serializers.py
@@ -35,3 +35,8 @@ class EventPartnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventPartner
         fields = '__all__'
+
+class MensBreakawaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MensBreakaway
+        fields = '__all__'
